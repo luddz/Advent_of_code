544ae796606379c677a021dde89dc80c07fe72e5
Ludvig Kindberg <chandazz96@gmail.com>
2022-12-10 16:57:19 +0100

    Day2 - version 2

--- 2022/day2/day-2.py
+++ 2022/day2/day-2.py
@@ -1,5 +1,19 @@
+from enum import Enum
+from dataclasses import dataclass
+
 example_input = "example.txt"
 test_input = "test.txt"
+
+class Scores(Enum):
+    ROCK = 1
+    PAPPER = 2
+    SCISSOR = 3
+
+@dataclass
+class Node:
+    iam: Scores
+    wins: Scores
+    loses: Scores
 
 input_lines = []
 file = test_input
@@ -9,48 +23,28 @@ with open(file, 'r') as f:
 total_score = 0
 win_score = 6
 draw_score = 3
-rock_score = 1
-papper_score = 2
-scissor_score = 3
 
-scores = [papper_score, rock_score, scissor_score] 
 def part_2():
     total_score = 0
     look_up = {
-        "B": papper_score,
-        "A": rock_score, 
-        "C": scissor_score,
+        "B": Node(Scores.PAPPER, Scores.ROCK, Scores.SCISSOR),
+        "A": Node(Scores.ROCK, Scores.SCISSOR, Scores.PAPPER), 
+        "C": Node(Scores.SCISSOR, Scores.PAPPER, Scores.ROCK),
         "X" : 0, #Lose score
         "Y": draw_score,
         "Z": win_score
     }
-
-    def get_loosing_play(oponent):
-        if oponent == papper_score:
-            return rock_score
-        elif oponent == rock_score:
-            return scissor_score
-        elif oponent == scissor_score:
-            return papper_score
     
-    def get_winnig_play(oponent):
-        if oponent == papper_score:
-            return scissor_score
-        elif oponent == rock_score:
-            return papper_score
-        elif oponent == scissor_score:
-            return rock_score
-
     for line in input_lines:
         plays = line.strip().split(" ")
         oponent, end_result = look_up[plays[0]], look_up[plays[1]] 
         total_score += end_result
         if end_result == 0:
-            total_score += get_loosing_play(oponent)
+            total_score += oponent.wins.value
         elif end_result == draw_score:
-            total_score += oponent
+            total_score += oponent.iam.value
         elif end_result == win_score:
-            total_score += get_winnig_play(oponent)
+            total_score += oponent.loses.value
 
     print (f"{total_score = }")
 
@@ -58,6 +52,9 @@ def part_2():
 
 def part_1():
     total_score = 0
+    rock_score = 1
+    papper_score = 2
+    scissor_score = 3
     look_up = {
         "B": papper_score,
         "Y": papper_score,
@@ -74,7 +71,6 @@ def part_1():
             you == scissor_score and oponent == papper_score:
             return win_score
         return 0
-
 
     for line in input_lines:
         plays = line.strip().split(" ")
