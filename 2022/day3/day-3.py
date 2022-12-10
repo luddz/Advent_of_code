93d9ff3abdbf6f370e510aa95f0bb7bc626784f9
Ludvig Kindberg <chandazz96@gmail.com>
2022-12-10 22:58:36 +0100

    Day 3 task 1

---
+++ 2022/day3/day-3.py
@@ -0,0 +1,31 @@
+import argparse
+from enum import Enum
+from dataclasses import dataclass
+
+example_input = "example.txt"
+test_input = "test.txt"
+
+def part_1():
+
+    return
+
+def part_2():
+    return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument("-e","--example", action='store_true')
+    args = parser.parse_args()
+
+    input_lines = []
+    file = test_input if not args.example else example_input
+    with open(file, 'r') as f:
+        input_lines = f.readlines()
+    
+    print("------------------------------------------------------------")
+    print("Part 1")
+    part_1(input_lines)
+
+    print("------------------------------------------------------------")
+    print("Part 2")
+    part_2(input_lines)
