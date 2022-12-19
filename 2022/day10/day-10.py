24710593add6db4b175fabc20f0c494c435a8b77
Ludvig Kindberg <chandazz96@gmail.com>
2022-12-19 23:03:09 +0100

    Day 10 done

--- 2022/day10/day-10.py
+++ 2022/day10/day-10.py
@@ -34,7 +34,42 @@ def part_1(input_lines):
     print(f"{signal_strenght = }")
 
 
+def print_sprite(sprite):
+    chunk_size = len(sprite)//6
+    res = [sprite[i:i+chunk_size] for i in range(0, len(sprite), chunk_size) ]
+    for l in res:
+        for c in l:
+            print(c, end="")
+        print()
+
+
+def get_sprite_span(draw_p, reg):
+    r_len = 40
+    for i in range(0,6):
+        if i * r_len <= draw_p < (i + 1) * r_len:
+            base = i * r_len 
+            return [base + (reg-1), base + reg, base + (reg + 1)]
+
+
+def draw(draw_p, reg):
+    return "#" if draw_p in get_sprite_span(draw_p, reg) else "."
+
+
 def part_2(input_lines):
+    sprite_as_line = ["." for _ in range(240)]
+    draw_pos = 0
+    register = 1
+    for instruction in  input_lines:
+        if instruction == "noop":
+            sprite_as_line[draw_pos] = draw(draw_pos, register)
+            draw_pos += 1
+        else:
+            for _ in range(2):
+                sprite_as_line[draw_pos] = draw(draw_pos, register)
+                draw_pos += 1
+
+            register += int(instruction.split(" ")[1])
+    print_sprite(sprite_as_line)
     return
 
 if __name__ == '__main__':
