74e0c601c27c888e051684d8e4c382677ca9239a
Ludvig Kindberg <chandazz96@gmail.com>
2023-12-05 22:05:21 +0100

    Day 4 part 2 done, could be optimized

--- 2023/day_4/day_4.py
+++ 2023/day_4/day_4.py
@@ -6,8 +6,7 @@ example_input = "example.txt"
 test_input = "test.txt"
 
 
-def part_1(input_lines):
-
+def part_1(input_lines: list[str]) -> None:
     total_score = 0
     for line in input_lines:
         card, numbers = line.split(":")
@@ -23,10 +22,34 @@ def part_1(input_lines):
         print(f"{card = } - {card_score = }")
         total_score += card_score
     print(f"{total_score = }")
-    return
 
 
-def part_2(input_lines):
+def num_wins(l1, l2):
+    return len([l_ for l_ in l1 if l_ in l2])
+
+
+def inner_recursion(cards, start_index):
+    c = cards[start_index]
+    wins = num_wins(c[0], c[1])
+    if wins == 0:
+        return 1
+    sub_cards = 0
+    for i in range(wins):
+        sub_cards += inner_recursion(cards, start_index + i + 1)
+    return sub_cards + 1
+
+
+def part_2(input_lines: list[str]) -> None:
+    cards = []
+    for line in input_lines:
+        card, numbers = line.split(":")
+        n1, n2 = numbers.split("|")
+        cards.append(([*filter(None, n1.split(" "))], [*filter(None, n2.split(" "))]))
+    total_cards = 0
+    for idx in range(len(cards)): 
+        total_cards += inner_recursion(cards, idx)
+
+    print(f"{total_cards = }")
     return
 
 
